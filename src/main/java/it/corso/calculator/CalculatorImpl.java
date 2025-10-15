bd9a7436ef0abae5e613f0b2e0f9260f8e0d8ebd
ppluto229 <ppluto229@gmail.com>
2025-10-15 11:17:18 +0200

    Update CalculatorImpl.java
    added hard-coded password to check

--- src/main/java/it/corso/calculator/CalculatorImpl.java
+++ src/main/java/it/corso/calculator/CalculatorImpl.java
@@ -2,6 +2,7 @@ package it.corso.calculator;
 
 public class CalculatorImpl implements Calculator {
 
+	// String SECRET_PASSWORD = "password1234"
 	public int add(int num1, int num2) {		
 		return num1+num2;
 		//return 4;
